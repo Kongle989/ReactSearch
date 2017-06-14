ef0193efc4126ef23aaf21857f543744bdf75b38
Kong Le <Kong Le>
2017-06-14 16:52:11 -0400

    modules, connection, port, server

---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require("express"),
+    bodyParser = require("body-parser"),
+    logger = require("morgan"),
+    mongoose = require("mongoose"),
+    request = require("request"),
+    app = express(),
+    url = 'mongodb://localhost/nytreact';
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+mongoose.Promise = Promise;
+app.use(express.static("public"));
+
+mongoose.connect(url);
+let db = mongoose.connection;
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
+});
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
+});
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("App running");
+});
