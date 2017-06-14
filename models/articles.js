43c503a5f8df3caa1e2063a27731c9875ca053e1
Kong Le <Kong Le>
2017-06-14 16:56:59 -0400

    article model

---
+++ models/articles.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+
+    ArticleSchema = new Schema({
+        title: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        },
+        saved: {
+            type: Boolean,
+            default: false,
+            required: true
+        }
+    });
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
